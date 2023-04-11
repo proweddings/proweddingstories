7f7542b23e39a10c9540763f4489c71366fc5699
proweddings <100475714+proweddings@users.noreply.github.com>
2023-04-12 00:09:26 +0530

    adding navigation to the lightbox

--- lightbox.js
+++ lightbox.js
@@ -3,6 +3,9 @@ const lightboxImage = document.querySelector(".lightbox-image");
 const caption = document.querySelector(".caption");
 const close = document.querySelector(".close");
 const galleryImages = document.querySelectorAll(".gallery-image");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+let currentIndex;
 
 galleryImages.forEach((image) => {
   image.addEventListener("click", () => {
@@ -21,3 +24,35 @@ lightbox.addEventListener("click", (event) => {
     lightbox.classList.add("hidden");
   }
 });
+
+galleryImages.forEach((image, index) => {
+    image.addEventListener("click", () => {
+      lightbox.classList.remove("hidden");
+      lightboxImage.src = image.dataset.src;
+      caption.innerHTML = image.alt;
+      currentIndex = index;
+    });
+  });
+  
+  function showImage(index) {
+    if (index < 0) index = galleryImages.length - 1;
+    if (index >= galleryImages.length) index = 0;
+    lightboxImage.src = galleryImages[index].dataset.src;
+    caption.innerHTML = galleryImages[index].alt;
+    currentIndex = index;
+  }
+  
+  prevBtn.addEventListener("click", () => {
+    showImage(currentIndex - 1);
+  });
+  
+  nextBtn.addEventListener("click", () => {
+    showImage(currentIndex + 1);
+  });
+  
+  document.addEventListener("keydown", (event) => {
+    if (lightbox.classList.contains("hidden")) return;
+    if (event.key === "ArrowLeft") showImage(currentIndex - 1);
+    if (event.key === "ArrowRight") showImage(currentIndex + 1);
+    if (event.key === "Escape") lightbox.classList.add("hidden");
+  });
